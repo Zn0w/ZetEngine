3df45a5d0b02b71129a7fc80f7a2b918c1416a80
Daniil Blagiy <blagiy.daniil@gmail.com>
2017-02-26 09:47:48 +0300

    Changed architecture + added fps handling

--- ZetEngine/src/com/znow/zetengine/test/Test.java
+++ ZetEngine/src/com/znow/zetengine/test/Test.java
@@ -1,12 +1,11 @@
 package com.znow.zetengine.test;
 
-import com.znow.zetengine.Game;
+import com.znow.zetengine.GameContainer;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Game game = new Game();
-		Thread gameThread = new Thread();
-		gameThread.start();
+		GameContainer gc = new GameContainer();
+		gc.start();
 	}
 }
